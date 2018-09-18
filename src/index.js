d4d586245f64c4ff1027104d3d25aa855e0d99d7
Cole Johnson <chj@purdue.edu>
2018-09-18 09:02:37 -0400

    Adds https redirect

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ import thunk from 'redux-thunk';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { PersistGate } from 'redux-persist/integration/react';
+import HttpsRedirect from 'react-https-redirect';
 
 import './index.css';
 import App from './App';
@@ -24,7 +25,9 @@ let persistor = persistStore(store)
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <App />
+      <HttpsRedirect>
+        <App />        
+      </HttpsRedirect>
     </PersistGate>
   </Provider>
   , document.getElementById('root'));
