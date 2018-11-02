b443002e3519fb2e4f477de443409ee9a8426a4b
Cole Johnson <chj@purdue.edu>
2018-11-02 10:13:27 -0400

    Changes infinite spinner

--- src/Components/SideBarView/SideBarView.js
+++ src/Components/SideBarView/SideBarView.js
@@ -19,7 +19,8 @@ class SideBarView extends Component {
         <div className="p-side_bar_view__art"></div>
         <div className="p-side_bar_view__container">
           <div className="p-side_bar_view__container_content">
-            { isFetching ? <InfiniteSpinner /> : children }
+            { children }
+            { isFetching ? <InfiniteSpinner /> : null }
           </div>
         </div>
 
