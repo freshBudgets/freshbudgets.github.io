12f9c855c861d353ad2026f1093d970e77489280
Cole Johnson <chj@purdue.edu>
2018-12-04 23:25:49 -0500

    Adds graph

--- src/Pages/Budget/Budget.js
+++ src/Pages/Budget/Budget.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 import NumberFormat from 'react-number-format';
+import { Line } from 'react-chartjs-2';
 
 import { apiGet, apiPost } from '../../Functions/api'
 import MobileNav from '../../Components/Nav/MobileNav';
@@ -131,6 +132,38 @@ class Budget extends PureComponent {
     const headerType = budget.currentAmount > budget.budgetLimit ? 'red' : 'green';
     const left = parseFloat(Math.round((budget.budgetLimit - budget.currentAmount) * 100) / 100).toFixed(2);
     const id = this.props.match.params.id;
+    const chartData = transactions.map((t, i) => {
+      return t.amount
+    })
+
+    const data = {
+      labels: chartData,
+      datasets: [
+        {
+          label: 'Transactions',
+          fill: false,
+          lineTension: 0,
+          backgroundColor: 'rgba(75,192,192,0.4)',
+          borderColor: 'rgba(105,203,228,1)',
+          borderCapStyle: 'butt',
+          borderDash: [],
+          borderDashOffset: 0.0,
+          borderJoinStyle: 'miter',
+          pointBorderColor: 'rgba(75,192,192,1)',
+          pointBackgroundColor: '#fff',
+          pointBorderWidth: 1,
+          pointHoverRadius: 5,
+          pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+          pointHoverBorderColor: 'rgba(220,220,220,1)',
+          pointHoverBorderWidth: 2,
+          pointRadius: 1,
+          pointHitRadius: 10,
+          data: chartData
+        }
+      ]
+    };
+
+    console.log(chartData);
 
     return (
       <div className="p-budget">
@@ -149,6 +182,11 @@ class Budget extends PureComponent {
           <div className="p-budget__transactions_header">
             <div>Transactions</div>
             <i className="fa fa-plus fa-sm" onClick={this.showNewTransactionModal}></i>
+          </div>
+          <div className="p-budget__graph">
+            <Line
+              data={data}
+            />
           </div>
           <TransactionTable transactions={transactions} updateTransactions={this.updateTransactions}/>
         </div>
